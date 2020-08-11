b97cb6da15583d57727f693277458427b8e6e4ec
iamernie8199 <iamernie8199@gmail.com>
2020-08-11 18:41:42 +0800

    Update data.py

--- data.py
+++ data.py
@@ -14,44 +14,97 @@ def CITCreat(con):
     cu.execute(create_cftc_cit_supplement_table_cmd)
     con.commit()
 
-def CITDownload(y):
-    t = datetime.datetime.today()
-    if y < 2006 or y > t.year:
-        return 0
-    print("下載" + str(y) + "年報告中...")
-    url = "https://www.cftc.gov/files/dea/history/dea_cit_xls_" + str(
-        y) + ".zip"
-    resp = requests.get(url)
-    file_path = os.path.join(os.path.basename(url))
+
+def FORCreat(con):
+    cu = con.cursor()
+    cu.execute(create_cftc_futures_only_reports_table_cmd)
+    con.commit()
+
+
+def Oldest(db):
+    if db == 'cftc_cit_supplement':
+        s = datetime.date(2006, 1, 1)
+    elif db == 'cftc_futures_only_reports':
+        s = datetime.date(1986, 1, 1)
+    return s
+
+
+def DataDownload(y, db):
+    if db == 'cftc_cit_supplement':
+        url = f"https://www.cftc.gov/files/dea/history/dea_cit_xls_{str(y)}.zip"
+    elif db == 'cftc_futures_only_reports':
+        if y <= 2003:
+            url = f"https://www.cftc.gov/files/dea/history/deafut_xls_{str(y)}.zip"
+        else:
+            url = f"https://www.cftc.gov/files/dea/history/dea_fut_xls_{str(y)}.zip"
+    print(f"下載{str(y)}年報告中...")
+    filename = Download(url)
+    return filename, os.path.basename(url)
+
+
+def Download(u):
+    resp = requests.get(u)
+    file_path = os.path.join(os.path.basename(u))
     with open(file_path, 'wb') as file:
         file.write(resp.content)
-
     with ZipFile(file_path) as zip_ref:
         filename = zip_ref.namelist()[0]
         zip_ref.extractall()
     return filename
 
 
-def CITProcess(p):
+def Process(p, s):
     df = pd.read_excel(p)
     df = df.drop(columns=['As_of_Date_In_Form_YYMMDD'])
     header = df.columns.tolist()
     header = header[1:2] + header[0:1] + header[2:]
     dict = {}
+    date = ['Report_Date_as_MM_DD_YYYY', 'Report_Date_as_YYYY_MM_DD']
     for h in header:
-        if h == 'Report_Date_as_MM_DD_YYYY' or h == 'Report_Date_as_YYYY_MM_DD':
+        if h in date:
             dict[h] = 'date'
         elif h[-1] == ' ':
             dict[h] = h[:-1].lower()
         elif h == 'Change_NonComm_Spead_All_NoCIT':
             dict[h] = 'Change_NonComm_Spread_All_NoCIT'.lower()
+        elif h == 'Change_in_NonComm_Spead_All':
+            dict[h] = 'Change_in_NonComm_Spread_All'.lower()
+        elif h == 'Traders_NonComm_Spead_Old':
+            dict[h] = 'Traders_NonComm_Spread_Old'.lower()
         else:
             dict[h] = h.lower()
     df = df[header]
     df = df.rename(columns=dict)
-    df = df[df.date > pd.Timestamp(start)]
+    df = df[df.date > pd.Timestamp(s)]
     df['date'] = df['date'].apply(lambda x: x.strftime("%Y-%m-%d"))
     return df
+
+
+def SQL(df, db):
+    engine = create_engine('postgres://' + pg_config['user'] + ':' +
+                           pg_config['password'] + '@' + pg_config['host'] +
+                           ':' + str(pg_config['port']) + '/' +
+                           pg_config['dbname'])
+    df.to_sql(db, engine, index=False, if_exists='append')
+
+
+def main(db, c):
+    c.execute(f"SELECT max(date) from {dbname}")
+    start = cur.fetchone()[0]
+    if start is None:
+        start = Oldest(dbname)
+    end = datetime.datetime.today()
+
+    for i in range(start.year, end.year + 1):
+        path, zpath = DataDownload(i, dbname)
+        if (not (path)):
+            print('Error')
+            continue
+        cit_df = Process(path, start)
+        SQL(cit_df, dbname)
+        print(f'{str(i)}年 Done')
+        os.remove(zpath)
+        os.remove(path)
 
 
 if __name__ == '__main__':
@@ -64,29 +117,10 @@ if __name__ == '__main__':
 
     # 建表用
     # CITCreat(conn)
+    FORCreat(conn)
 
     # 取得最新日期
-    cur.execute("SELECT max(date) from cftc_cit_supplement")
-    start = cur.fetchone()[0]
-    if start is None:
-        start = datetime.date(2006, 1, 1)
-    end = datetime.datetime.today()
-
-    for i in range(start.year, end.year + 1):
-        path = CITDownload(i)
-        if (not (path)):
-            print('Error')
-            continue
-        cit_df = CITProcess(path)
-        engine = create_engine('postgres://' + pg_config['user'] + ':' +
-                               pg_config['password'] + '@' +
-                               pg_config['host'] + ':' +
-                               str(pg_config['port']) + '/' +
-                               pg_config['dbname'])
-        cit_df.to_sql('cftc_cit_supplement',
-                  engine,
-                  index=False,
-                  if_exists='append')
-        print(str(i) + '年 Done')
-        os.remove("dea_cit_xls_" + str(i) + ".zip")
-        os.remove(path)
+    """dbname = 'cftc_cit_supplement'
+    main(dbname, cur)"""
+    dbname = 'cftc_futures_only_reports'
+    main(dbname, cur)
